e8b573eb0b87f69fb1f8074f5ab4774003760911
Yashvi Nathwani <nathwaniyashvi@gmail.com>
2021-07-18 03:51:08 +0530

    Login request

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config')
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
@@ -16,6 +20,65 @@ router.get('/', auth, async (req, res) => {
         console.log(err.message);
         res.status(500).send('Server Error');
     }
-})
+});
+
+//@route    POST api/auth
+//@desc     Authenticate user & get token
+//@access   Public
+router.post('/', [
+    check('email', 'Email is required').isEmail(),
+    check('password', 'Password is required').exists()
+],
+    async (req, res) => {
+        // console.log(req.body)
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        try {
+
+            //Check if user already exists
+            let user = await User.findOne({ email });
+
+            // If user doesn't exist, send error
+            if (!user) {
+                return res.status(400).send({ errors: [{ msg: 'Invalid Credentials' }] });
+            }
+
+            // If user exist, then check if the entered password is same as the user's password in database
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if(!isMatch){
+                return res.status(400).send({ errors: [{ msg: 'Invalid Credentials' }] });
+            }
+
+            // Implement JWT
+            const payload = {
+                user : {
+                    id : user.id
+                }
+            }
+
+            jwt.sign(
+                payload, 
+                config.get('jwtSecret'),
+                { expiresIn : 360000 },
+                (err, token) => {
+                    if(err) throw err;
+                    res.json({ token })
+                }
+            )
+        }
+        catch (err) {
+            console.log(err.message);
+            res.status(500).send('Server Error');
+        }
+
+    }
+);
 
 module.exports = router;
