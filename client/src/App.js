21c503048cc3a52a9e74c9bd71197f16aac65e24
Yashvi Nathwani <nathwaniyashvi@gmail.com>
2022-01-31 01:57:40 +0530

    Protected route

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 // Redux provider to provide our entire app the store
 import { Provider } from 'react-redux';
 import store from './store';
@@ -36,6 +38,11 @@ const App = () => {
                                 component={Register}
                             />
                             <Route exact path='/login' component={Login} />
+                            <PrivateRoute
+                                exact
+                                path='/dashboard'
+                                component={Dashboard}
+                            />
                         </Switch>
                     </section>
                 </Fragment>
