12a2bb5741df36a700895c0115a080b37d199436
Yashvi Nathwani <nathwaniyashvi@gmail.com>
2022-01-30 18:18:53 +0530

    Alert component and Action call

--- client/src/App.js
+++ client/src/App.js
@@ -5,27 +5,33 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 // Redux provider to provide our entire app the store
 import { Provider } from 'react-redux';
 import store from './store';
 
 const App = () => {
-  return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Route exact path='/' component={Landing} />
-          <section className='container'>
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-            </Switch>
-          </section>
-        </Fragment>
-      </Router>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Router>
+                <Fragment>
+                    <Navbar />
+                    <Route exact path='/' component={Landing} />
+                    <section className='container'>
+                        <Alert />
+                        <Switch>
+                            <Route
+                                exact
+                                path='/register'
+                                component={Register}
+                            />
+                            <Route exact path='/login' component={Login} />
+                        </Switch>
+                    </section>
+                </Fragment>
+            </Router>
+        </Provider>
+    );
 };
 
 export default App;
--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,13 +1,20 @@
-import uuid from 'uuid';
+import { v4 as uuid } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 // Action creator
-export const setAlert = (msg, alertType) => (dispatch) => {
-    const id = uuid.v4();
+export const setAlert =
+    (msg, alertType, timeout = 5000) =>
+    (dispatch) => {
+        const id = uuid();
 
-    // Dispatch an action
-    dispatch({
-        type: SET_ALERT,
-        payload: { id, msg, alertType },
-    });
-};
+        // Dispatch an action
+        dispatch({
+            type: SET_ALERT,
+            payload: { id, msg, alertType },
+        });
+
+        setTimeout(
+            () => dispatch({ type: REMOVE_ALERT, payload: id }),
+            timeout
+        );
+    };
