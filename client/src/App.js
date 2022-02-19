f44cedfd82ab9282a43eda7218c62ba531b0fc1f
Yashvi Nathwani <nathwaniyashvi@gmail.com>
2022-02-20 04:53:51 +0530

    Edit Profile

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux provider to provide our entire app the store
 import { Provider } from 'react-redux';
@@ -48,6 +49,11 @@ const App = () => {
                                 exact
                                 path='/create-profile'
                                 component={CreateProfile}
+                            />
+                            <PrivateRoute
+                                exact
+                                path='/edit-profile'
+                                component={EditProfile}
                             />
                         </Switch>
                     </section>
