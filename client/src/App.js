dee37a20dbe90e856dbc36c1ef59779a61c144dc
Yashvi Nathwani <nathwaniyashvi@gmail.com>
2022-02-20 02:33:44 +0530

    CreateProfile Component

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux provider to provide our entire app the store
 import { Provider } from 'react-redux';
@@ -42,6 +43,11 @@ const App = () => {
                                 exact
                                 path='/dashboard'
                                 component={Dashboard}
+                            />
+                            <PrivateRoute
+                                exact
+                                path='/create-profile'
+                                component={CreateProfile}
                             />
                         </Switch>
                     </section>
