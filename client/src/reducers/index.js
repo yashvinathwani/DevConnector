0d7e721cc270f9687201d376d349a91f3ac502fd
Yashvi Nathwani <nathwaniyashvi@gmail.com>
2022-02-27 22:39:07 +0530

    Post Reducer, Action and Initial Component

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import profile from './profile';
+import post from './post';
 
 // Combine the reducers
 export default combineReducers({
     alert,
     auth,
     profile,
+    post,
 });
