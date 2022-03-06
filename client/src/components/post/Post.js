4bd84a428398e42fba9b4834048969a60c3dffe2
Yashvi Nathwani <nathwaniyashvi@gmail.com>
2022-03-07 00:44:37 +0530

    Adding Comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getPost } from '../../actions/post';
 import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
 // import Spinner from '../layout/Spinner'
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -19,6 +20,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
                 Back To Posts
             </Link>
             <PostItem post={post} showActions={false} />
+            <CommentForm postId={post._id} />
         </Fragment>
     );
 };
