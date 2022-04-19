e214a9e0aa3d0a4fa3b7177071c1db5e07faea1a
Yashvi Nathwani <nathwaniyashvi@gmail.com>
2022-04-19 23:36:52 +0530

    Edit profile route changes

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -47,7 +47,7 @@ const EditProfile = ({
             instagram:
                 loading || !profile.social ? '' : profile.social.instagram,
         });
-    }, [loading, getCurrentProfile, profile]);
+    }, [loading, getCurrentProfile]);
 
     const {
         company,
